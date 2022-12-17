2fb88262df0c44179f0bfbdadb1efec042b9595a
akrjuchkov <al.krjuchkov@cft.ru>
2022-12-17 10:19:59 +0600

    Добавил доходность

--- controller.py
+++ controller.py
@@ -26,11 +26,13 @@ class Controller:
         final_text = ''
         for id, weight in client.personal_funds.items():
             fund_info = self._search_fund_by_id(self.funds, id)
-            final_text += f'{int(yearly / 60 * 0.3 / 12 / fund_info.price)} паев фонда {fund_info.full_name} \n'
+            final_text += f'{int(yearly / 60 * 0.3 / 12 / fund_info.price)} паев фонда {fund_info.full_name}' \
+                          f'с доходностью {fund_info.average_profit} %\n'
         return f'\nДо пенсии осталось {client.years_before_retirement} лет\n' \
                f'Ежемесячные траты составляют {client.expenses_per_month} рублей\n' \
                f'Без учета сложного процента надо откладывать {int(fire_number / client.years_before_retirement)} рублей в год\n' \
-               f'С учетом сложного процента надо откладывать {int(yearly)} рублей в год\n\n' \
+               f'С учетом сложного процента надо откладывать {int(yearly)} рублей в год\n' \
+               f'Ожидаемая доходность: {average_profit}\n\n' \
                f'Для соответствия инвестиционному плану в этом месяце надо купить: \n' \
                f'{final_text}'
 
