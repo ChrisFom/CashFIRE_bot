346ce1fd946bb5e0a7433110e30c27771755a790
akrjuchkov <al.krjuchkov@cft.ru>
2022-12-17 10:55:29 +0600

    Исправил текст

--- controller.py
+++ controller.py
@@ -27,12 +27,12 @@ class Controller:
         for id, weight in client.personal_funds.items():
             fund_info = self._search_fund_by_id(self.funds, id)
             final_text += f'{int(yearly / 60 * 0.3 / 12 / fund_info.price)} паев фонда {fund_info.full_name}' \
-                          f' с доходностью {fund_info.average_profit*100} %\n'
+                          f' ( доходность {round(fund_info.average_profit*100, 1)} % )\n'
         return f'\nДо пенсии осталось {client.years_before_retirement} лет\n' \
                f'Ежемесячные траты составляют {client.expenses_per_month} рублей\n' \
                f'Без учета сложного процента надо откладывать {int(fire_number / client.years_before_retirement)} рублей в год\n' \
                f'С учетом сложного процента надо откладывать {int(yearly)} рублей в год\n' \
-               f'Ожидаемая доходность: {average_profit*100} % в год\n\n' \
+               f'Ожидаемая доходность: {round(average_profit*100, 1)} % в год\n\n' \
                f'Для соответствия инвестиционному плану в этом месяце надо купить: \n' \
                f'{final_text}'
 
